b53366e160f6d5feb0f3238015764630de7f984e
DaviihRodrigueSs <davi.devrodrigues@gmail.com>
2024-03-06 13:04:16 -0300

    Matrizes

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+using System.Globalization;
+namespace Matrizes1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //indicação de um arranjo bidimensional [,]
+
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine(mat.Length);
+
+            Console.WriteLine((mat.Rank));
+
+            Console.WriteLine(mat.GetLength(0));
+
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
